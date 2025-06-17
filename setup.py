8b4a2f331d98a2514958391779046f53354b8f5a
Yan Bai <bayan@nvidia.com>
2025-06-17 01:36:01 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-from setuptools import find_packages, setup
+# Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
 
-setup(
-    name="mbridge",
-    version="0.1.0",
-    description="Bridge between Reinforcement Learning and Megatron-Core",
-    author="Yan Bai",
-    author_email="bayan@nvidia.com",
-    packages=find_packages(include=['mbridge', 'mbridge.*']),
-    install_requires=[
-        "megatron-core>=0.12.0",
-        "transformers",
-        "safetensors",
-    ],
-    python_requires=">=3.8",
-)
+from setuptools import setup
+
+setup()
