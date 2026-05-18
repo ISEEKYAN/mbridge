37b2b26b0547b45d5ab9b3809c3bf9db8c982b71
Yan Bai <baiyan1996@icloud.com>
2026-05-19 00:44:58 +0800

    Revert "fix dynamic-cp (#139)"
    This reverts commit 4176f5971548610142cee607fa2132ca16f21ce7.

--- mbridge/models/qwen3_vl/rope_utils.py
+++ mbridge/models/qwen3_vl/rope_utils.py
@@ -118,7 +118,6 @@ class Qwen3VLMultimodalRotaryEmbedding(nn.Module):
             Tensor: Embeddings after applying RoPE.
         """
         seq = position_ids.to(device=self.inv_freq.device, dtype=self.inv_freq.dtype)
-        cp_group = kwargs.get("cp_group", parallel_state.get_context_parallel_group())
 
         if self.seq_len_interpolation_factor is not None:
             seq *= 1 / self.seq_len_interpolation_factor
@@ -137,12 +136,14 @@ class Qwen3VLMultimodalRotaryEmbedding(nn.Module):
         # shape (seq_length, bs, 1, 2 * dim)
         emb = emb[..., None, :].transpose(0, 1).contiguous()
         if (
-            cp_group.size() > 1
+            parallel_state.get_context_parallel_world_size() > 1
             and not self.is_thd_format
         ):
             # slice rotary_pos_emb along sequence dimension and select the parition of the current
             # CP rank
-            emb = get_pos_emb_on_this_cp_rank(emb, 0, cp_group)
+            emb = get_pos_emb_on_this_cp_rank(
+                emb, 0, parallel_state.get_context_parallel_group()
+            )
         return emb
 
 
